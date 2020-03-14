f5e5014beb3c31634363ed409dedfa3705ab7f5e
ghalls99 <54293763+ghalls99@users.noreply.github.com>
2020-03-14 10:14:15 -0600

    Add files via upload

--- garrettpage.js
+++ garrettpage.js
@@ -1,8 +1,8 @@
-$('a.rick-link').click(function(e)) {
+$('a.rick-link').click(function(e) {
     e.preventDefault();
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 
-}
+});
