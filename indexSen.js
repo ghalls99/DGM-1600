95a1b835405616e093e5a43897b4693cc1f43d0a
garrett <garrett@hallsnet.com>
2020-04-30 18:47:28 -0600

    updated a lot

--- indexSen.js
+++ indexSen.js
@@ -1,39 +1,68 @@
 import { senators } from './senators.js'
 
-const senatorDiv = document.querySelector('.senators')
+const senatorDiv = document.querySelector('.senators');
 
 
-function populateSenatorDiv() {
-    senators.forEach(senator => {
-        let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+function populateSenatorDiv(senator) {
+    senator.forEach(item => {
+        let middleName = item.middle_name ? ` ${item.middle_name} ` : ` `;
 
-        let senFigure = document.createElement('figure')
-        let figImg = document.createElement('img')
-        let figCaption = document.createElement('figcaption')
+        let senFigure = document.createElement('figure');
+        let figImg = document.createElement('img');
+        let figCaption = document.createElement('figcaption');
 
 
-        figImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
-        figCaption.textContent = `${senator.first_name}${middleName}${senator.last_name}`
+        figImg.src = `https://www.govtrack.us/static/legislator-photos/${item.govtrack_id}-200px.jpeg`;
+        figCaption.textContent = `${item.first_name}${middleName}${item.last_name}`;
 
-        senFigure.appendChild(figImg)
-        senFigure.appendChild(figCaption)
-        senatorDiv.appendChild(senFigure)
+        senFigure.appendChild(figImg);
+        senFigure.appendChild(figCaption);
+        senatorDiv.appendChild(senFigure);
     })
-    main.appendChild(senatorDiv)
+
 }
+
+const loadAllSenators = () => {
+    populateSenatorDiv(senators);
+}
+
+showAll.addEventListener('click', () => {
+    senatorDiv.innerHTML = '';
+
+    loadAllSenators();
+})
+
+filterRepPartyResults.addEventListener('click', () => {
+    let filterRep = filterSenators('party', 'R');
+    senatorDiv.innerHTML = '';
+
+    populateSenatorDiv(filterRep);
+    console.log(populateSenatorDiv(filterRep));
+})
+
+filterDemPartyResults.addEventListener('click', () => {
+    let filterRep = filterSenators('party', 'D');
+    senatorDiv.innerHTML = '';
+
+    populateSenatorDiv(filterRep);
+})
+
+
+filterIndiPartyResults.addEventListener('click', () => {
+    let filterRep = filterSenators('party', 'ID');
+    senatorDiv.innerHTML = '';
+
+    populateSenatorDiv(filterRep);
+})
+
 
 const filterSenators = (prop, value) => {
     return senators.filter(senator => {
-        return senator[prop] === value
+        return senator[prop] === value;
     })
 }
-
-/*console.log(filterSenators('party', 'R'))
-console.log(filterSenators('party', 'D'))
-console.log(filterSenators('party', 'ID'))*/
-
 const senatorNames = senators.map(senator => {
-    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `;
     return {
         id: senator.id,
         name: `${senator.first_name}${middleName}${senator.last_name}`,
@@ -43,4 +72,4 @@ const senatorNames = senators.map(senator => {
 
 console.log(senatorNames)
 
-populateSenatorDiv()
+loadAllSenators();
